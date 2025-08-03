a9166d0ca6220fd67807dae1ac5e573a521ef70c
vuxnq <vuxnqu@gmail.com>
2025-08-03 12:21:22 +0200

    bruh lastfm

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ ACTION=${1:-backup}
 BACKUP_DIR="/mnt/backups"
 
 RESTIC_REPOSITORY="$BACKUP_DIR/restic"
-TO_BACKUP=("/mnt/music" "/mnt/immich" "copyparty/data" "immich/data" "navidrome/data")
+TO_BACKUP=("/mnt/cloud" "/mnt/music" "/mnt/immich" "copyparty/data" "immich/data" "navidrome/data")
 
 load_restic_password() {
   if [[ -f .env.secret ]]; then
